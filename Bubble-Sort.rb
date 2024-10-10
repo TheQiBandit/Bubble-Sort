e1b1a87a507bd5e8dba4ab9d968464658c8d14f6
TheQiBandit <164599241+TheQiBandit@users.noreply.github.com>
2024-10-10 17:25:36 +0300

    Add another loop to control no. of iterations
    Added another loop to complete the number of times
    the array is sorted to completetion.

--- Bubble-Sort.rb
+++ Bubble-Sort.rb
@@ -6,14 +6,20 @@ def bubble_sort(arr)
   # variable to control how many times to run the loop
   n = arr.length - 1
 
-  # run the loop n times to pass through the array and switch elements
-  n.times do |x|
+  #variable to control how many iterations until array is sorted
+  iterations = arr.length - 1
 
-    # if the element to the right is smaller then switch elements
-    if arr[x] > arr[x + 1]
-      arr[x], arr[x + 1] = arr[x + 1], arr[x]
-    end # end if
-  end # end times
+  while iterations >= 1
+  # run the loop n times to pass through the array and switch elements
+    n.times do |x|
+
+      # if the element to the right is smaller then switch elements
+      if arr[x] > arr[x + 1]
+        arr[x], arr[x + 1] = arr[x + 1], arr[x]
+      end # end if
+    end # end times
+    iterations -= 1
+  end
  arr
 end
 
